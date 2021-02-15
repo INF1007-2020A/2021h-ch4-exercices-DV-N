a9b4640d3a19542c521fba553ba28b15d8246c83
DV-N <dangvinh.ng@gmail.com>
2021-02-14 22:35:56 -0500

    Révision 2/14/2021

--- exercice.py
+++ exercice.py
@@ -11,9 +11,9 @@ def remove_third_char(string: str) -> str:  # str[0:2]+[3:]
     return string[0:2] + string[3:]
 
 
-def replace_char(string: str, old_char: str, new_char: str) -> str:
-    position_of_old_char = string.index(old_char)  # donne position/l'index qui est un int, alternative for loop and if statement pcq comme c'est pas capable d'identifier pls old_char
-    string = string[:position_of_old_char] + new_char + string[position_of_old_char + 1:]
+def replace_char(string: str, old_char: str, new_char: str) -> str: #À refaire
+    position_of_old_char = string.index(old_char)  # donne position/l'index qui est un int. Alternative for loop and if statement pcq comme c'est, pas capable d'identifier pls old_char
+    string = string[:position_of_old_char] + new_char + string[position_of_old_char+1 :]
     return string
 
 def get_number_of_char(string: str, char: str) -> int:
@@ -31,12 +31,11 @@ def get_number_of_char(string: str, char: str) -> int:
 
 def get_number_of_words(sentence: str, word: str) -> int:
     number_of_word = 0
-    sentence = sentence.split()
+    sentence = sentence.split() #sépare sentence en une liste de mots ['ceci', 'est', 'une', 'phrase', 'test']
     for i in sentence:  # pourquoi ecq for word in sentence: ne fonctionne pas
         if i == word:
             number_of_word += 1
     return number_of_word
-
 
 def main() -> None:
     chaine = "Bonjour!"
@@ -51,7 +50,7 @@ def main() -> None:
     chaine = "hello world!"
     print(f"On remplace le caractère w par le caractère z dans la chaine: {chaine}. Résultat : {replace_char(chaine, 'w', 'z')}")
 
-    print(f"Le nombre d'occurrence de l dans 'hello world' est : {get_number_of_char(chaine, 'l')}")  # hello ou hello zworld
+    print(f"Le nombre d'occurrence de l dans 'hello world' est : {get_number_of_char(chaine, 'l')}")
 
     chaine = "Baby shark doo doo doo doo doo doo"
     print(f"L'occurence du mot doo dans la chaine {chaine} est: {get_number_of_words(chaine, 'doo')}")
